2c32e1fe08f30580d9d6c58ad2f8b6630368699f
EC2 Default User <ec2-user@ip-172-31-57-120.ec2.internal>
2022-06-28 05:50:26 +0000

    csvファイル名が固定化されている点を変更

--- memo.rb
+++ memo.rb
@@ -2,14 +2,14 @@ require "csv"
 puts "以下の数字を入力してください"
 puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 
-num_type = gets.to_i                                     #数字を押される
+num_type = gets.to_i                                     #数字を押される 
 if num_type == 1                                         #1出力
     puts "拡張子を除いたファイルを入力してください"
-    file_name = gets.chomp                               #ファイル名出力
-    CSV.open("#{file_name}.csv","w+") do |contents|
+    file_name = gets.chomp                               #改善点　ファイル名出力
+    CSV.open("#{file_name}.csv","w+") do |contents|      #改善点
         puts "メモしたい内容を記入してください"
         puts "完了したら Ctr + D を押します"
-        contents << [$stdin.read]                         #内容出力
+        contents << [$stdin.read]                        #内容出力
         end
 elsif num_type == 2
         puts "既存のメモを編集します"
