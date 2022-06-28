478d3e8b84bac0d85eb28c3c1b5290f94dc53d6d
EC2 Default User <ec2-user@ip-172-31-57-120.ec2.internal>
2022-06-28 06:39:40 +0000

    メモの編集機能を追加

--- memo.rb
+++ memo.rb
@@ -7,12 +7,17 @@ if num_type == 1                                         #1出力
     puts "拡張子を除いたファイルを入力してください"
     file_name = gets.chomp                               #改善点　ファイル名出力
     CSV.open("#{file_name}.csv","w+") do |contents|      #改善点
-        puts "メモしたい内容を記入してください"
-        puts "完了したら Ctr + D を押します"
+        puts "メモしたい内容を入力してください"
+        puts "完了したら Ctr + D を押下してください"
         contents << [$stdin.read]                        #内容出力
         end
 elsif num_type == 2
-        puts "既存のメモを編集します"
+    puts "既存の拡張子を除いたファイル名を入力してください。"
+    file_name = gets.chomp
+    CSV.open("#{file_name}.csv","a+") do |contents_add|
+        puts "内容を編集後　Ctr ＋ Dを押下してください"
+        contents_add << [$stdin.read]
+    end
 else
         puts "1と2を入力してください"
 end
